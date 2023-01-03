9570e7675ea0e044b51927c2dcc525df148bada8
cliffgor <clierozigy@gmail.com>
2023-01-03 15:34:09 +0200

    Routes created a folder and the routes i am going to use in the backend

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 import bodyParser from 'body-parser'
+import clientRoutes from "routes/clients.js"
 import cors from 'cors'
 import dotenv from 'dotenv'
 import express from 'express'
+import generalRoutes from "routes/general.js"
 import helmet from 'helmet'
+import managementRoutes from "routes/managements.js"
 import mongoose from 'mongoose'
 import morgan from 'morgan'
+import salesRoutes from "routes/sales.js"
 
 // server Configurations 
 
